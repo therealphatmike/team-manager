c823edcc4744379de56d87c7790b6f23026a383d
T Michael Smith <t.michael.smith@toyota.com>
2023-03-12 09:44:05 -0500

    cleaned up team resource to use other resources and filter out audit data

--- app/Http/Resources/TeamResource.php
+++ app/Http/Resources/TeamResource.php
@@ -14,6 +14,12 @@ class TeamResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'website' => $this->website,
+            'cars' => CarResource::collection($this->whenLoaded('cars')),
+            'drivers' => DriverResource::collection($this->whenLoaded('drivers')),
+        ];
     }
 }
